3ff4be1003426c71b8e96e09d4799e4ec7939c3b
atharv02ag <atharvagarwal2718@gmail.com>
2025-05-14 18:03:37 +0530

    signout added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import "./Home.css";
 import { BarChart2, FileText, HelpCircle, LayoutGrid } from "lucide-react";
 import { Link } from "react-router-dom";
-import { useGoogleLogin } from "@react-oauth/google";
+import { useGoogleLogin, googleLogout } from "@react-oauth/google";
 import axios from 'axios';
-import { useState } from 'react';
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { useState, useEffect } from 'react';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 
 export default function Home() {
 
@@ -13,25 +20,60 @@ export default function Home() {
 
     const googleLogin = useGoogleLogin({
         onSuccess: async (tokenResponse) => {
-            console.log(tokenResponse);
+            //console.log(tokenResponse);
             const userInfo = await axios.get(
                 'https://www.googleapis.com/oauth2/v3/userinfo',
                 { headers: { Authorization: `Bearer ${tokenResponse.access_token}` } },
             );
 
             console.log(userInfo.data);
+            localStorage.setItem("user", JSON.stringify(userInfo.data));
+            localStorage.setItem("access_token", tokenResponse.access_token);
+
             setProfilePic(userInfo.data.picture);
             setLoggedIn(1);
         },
         onError: errorResponse => console.log(errorResponse),
     });
 
-    const avatarTag = (pictureSrc)=>{
-        return (<Avatar className="w-8 h-8">
-                    <AvatarImage src={pictureSrc} />
-                </Avatar>
-                );
+    const handleLogout = ()=>{
+        googleLogout();
+        localStorage.removeItem("user");
+        localStorage.removeItem("access_token");
+        setLoggedIn(0);
+        setProfilePic('');
     }
+
+
+    const avatarTag = (pictureSrc)=>{
+        return(
+            <DropdownMenu>
+            <DropdownMenuTrigger><Avatar className="w-8 h-8 cursor-pointer">
+                                <AvatarImage src={pictureSrc} />
+                                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem onClick={handleLogout}>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+            </DropdownMenu> 
+        )
+    }
+
+    useEffect(()=>{
+        const savedUser = localStorage.getItem("user");
+        const savedToken = localStorage.getItem("access_token");
+
+        if (savedUser && savedToken) {
+            const userInfoData = JSON.parse(savedUser);
+            setProfilePic(userInfoData.picture);
+            setLoggedIn(1);
+        }
+        else{
+            setProfilePic('');
+            setLoggedIn(0);
+        }
+    },[]);
 
     return (
         <>
@@ -56,8 +98,10 @@ export default function Home() {
                     <p className="main-subtitle">
                         Upload, Analyze, and Interact with Academic Papers—Powered by AI
                     </p>
-                    <button className="start-button">Get Started</button>
-
+                    <div className="start-button-wrapper">
+                        <button className="start-button">Get Started</button>
+                    </div>
+                    
                     <div className="features">
                         <div className="feature-card">
                             <FileText className="icon" />
